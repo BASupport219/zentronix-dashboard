89dd27e953878b8dde7d7be498d3aa8d857f638c
BASupport219 <ba6985453@gmail.com>
2026-04-05 14:56:59 +0800

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,8 @@
+// This file replaces the .env file.
+// It securely holds the credentials separate from the HTML, 
+// but allows Chrome to read it without needing localhost!
+
+const ZentroNixConfig = {
+    USERNAME: "BAotp",
+    PASSWORD: "BaSupp0rt@2026"
+};
